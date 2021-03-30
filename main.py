a5b56fa55b353d8d6724d2f151cb76ba92ab0b67
hisui0 <lassloshbf@gmail.com>
2021-03-31 05:17:37 +0900

    fix

--- main.py
+++ main.py
@@ -121,49 +121,50 @@ def all_sns():
         template=CarouselTemplate(
             columns=[
                 CarouselColumn(
-                    thumbnail_image_url='https://reiwacity-linebot.s3-ap-northeast-1.amazonaws.com/164805270_470119047735471_3970313014321092495_n.jpg',
-                    title='ホームページ',
-                    text='記念日を入れたくなったらここかな。',
+                    thumbnail_image_url='https://reiwacity-linebot.s3-ap-northeast-1.amazonaws.com/166060251_156255479701750_7248689856255699436_n.jpg',
+                    title='note「令和市だより」',
+                    text='もう説明は意味をなさなくなってきたけれど、行動の背景を知りたくなるときもあるよね。',
                     actions=[
                         URIAction(
-                            label='Go',
-                            uri='https://previewer.adalo.com/7e89d6b4-efb5-435c-8ea3-3822f4a7c5c0/'
+                            label='鑑賞する',
+                            uri='https://note.com/reiwacity/m/m5467a2ca46c6'
                         )
                     ]
                 ),
                 CarouselColumn(
                     thumbnail_image_url='https://reiwacity-linebot.s3-ap-northeast-1.amazonaws.com/165932370_775317893098235_6080044181924237203_n.jpg',
                     title='twitter',
-                    text='フォローしておくとたまにtwitterのタイムラインに現れるよ。',
+                    text='twitter上でも令和市の気配を感じたくなったらフォローしといて。',
                     actions=[
                         URIAction(
-                            label='Go',
+                            label='鑑賞する',
                             uri='https://twitter.com/ReiwaCityNews'
                         )
                     ]
                 ),
+
                 CarouselColumn(
                     thumbnail_image_url='https://reiwacity-linebot.s3-ap-northeast-1.amazonaws.com/166060251_156255479701750_7248689856255699436_n.jpg',
-                    title='note「令和市だより」',
-                    text='もう説明は不必要な時代だけれど、背景の思いを知りたくなるときもあるよね。',
+                    title='令和市民大学',
+                    text='ありのままに見えている世界を共有したくなったらここだな。誰かと話すって、いいよね。',
                     actions=[
                         URIAction(
-                            label='Go',
-                            uri='https://note.com/reiwacity/m/m5467a2ca46c6'
+                            label='鑑賞する',
+                            uri='https://peraichi.com/landing_pages/view/r-university'
                         )
                     ]
                 ),
                 CarouselColumn(
-                    thumbnail_image_url='https://reiwacity-linebot.s3-ap-northeast-1.amazonaws.com/166060251_156255479701750_7248689856255699436_n.jpg',
-                    title='令和市民大学',
-                    text='ありのままに見えている世界を共有したくなったらここかな。',
+                    thumbnail_image_url='https://reiwacity-linebot.s3-ap-northeast-1.amazonaws.com/164805270_470119047735471_3970313014321092495_n.jpg',
+                    title='ホームページ',
+                    text='記念日を入れたくなったらここだね。このHP、リモコンみたいだよね。',
                     actions=[
                         URIAction(
-                            label='Go',
-                            uri='https://peraichi.com/landing_pages/view/r-university'
+                            label='鑑賞する',
+                            uri='https://previewer.adalo.com/7e89d6b4-efb5-435c-8ea3-3822f4a7c5c0/'
                         )
                     ]
-                )
+                )               
             ]
         )
     )
