2baf703d3f2f5c23c3ae3cdbcd0a4848291b9333
hisui0 <lassloshbf@gmail.com>
2021-03-31 02:35:20 +0900

    fix

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ from linebot.exceptions import (
 from linebot.models import (
     MessageEvent, TextMessage, TextSendMessage,FlexSendMessage,MessageAction,
     QuickReply, PostbackEvent,CarouselTemplate, ImageCarouselTemplate,
-    CarouselColumn, ImageCarouselColumn,QuickReplyButton
+    CarouselColumn, ImageCarouselColumn,QuickReplyButton, URIAction
 )
 
 app = Flask(__name__)
