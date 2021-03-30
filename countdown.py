dbec8f10af4f004eb5ae316dc365f9fded2dcb18
hisui0 <lassloshbf@gmail.com>
2021-03-31 03:31:02 +0900

    fix

--- countdown.py
+++ countdown.py
@@ -5,5 +5,5 @@ def countdown():
 	dt3 = dt2 - dt1
 	days = dt3.days + 2
 	
-	str_days = "今日はETA" + str(days) + "\n令和市が消滅するまであと" + str(days)"日だよ"
+	str_days = "今日はETA" + str(days) + "\n令和市が消滅するまであと" + str(days) +"日だよ"
 	return str_days
