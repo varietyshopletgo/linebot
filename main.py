e4949e030bcbdfc52d5ef476374e16f6fccb104f
hisui0 <lassloshbf@gmail.com>
2021-03-31 22:35:47 +0900

    fix

--- main.py
+++ main.py
@@ -66,7 +66,7 @@ def handle_message(event):
         yourname = username_search(event)
         result1 = countdown.countdown()
         str_day = countdown.countdown2()
-        result2 = countdown.cd_sendmsg(str_day,yourname)
+        result2 = countdown.cd_sendmsg("4/3","name") #(str_day,yourname)
         line_bot_api.reply_message(event.reply_token,result1)
     elif event.message.text == "近々なにかある？":
         result1 = timetree.get_timetree()
