c3d3a5244278d00d2811e656db53265d257fa1ca
hisui0 <lassloshbf@gmail.com>
2021-03-31 22:41:55 +0900

    fix

--- countdown.py
+++ countdown.py
@@ -27,7 +27,7 @@ def add_centered_text(base_img, text, font_path, font_size, font_color, height):
     draw.text(((base_img.size[0] - draw.textsize(text, font=font)[0]) / 2, height), text, font_color, font=font,)
     return base_img
 
-def cd_imagemaker(str_day, yourname):
+def cd_imagemaker(): #str_day, yourname
     #背景とアイコンのファイルを指定
     ogp_base_img_path = 'bg.png'
 
@@ -35,8 +35,8 @@ def cd_imagemaker(str_day, yourname):
     font_bold_path = "NotoSansJP-Bold.otf"
     font_medium_path = "NotoSansJP-Medium.otf"
 
-    msg1 = 'ETA-' + str_days
-    msg2 = 'for' + yourname
+    msg1 = 'ETA-'# + str_days
+    msg2 = 'for' #+ yourname
     base_img = Image.open(ogp_base_img_path).copy()
     base_img = add_centered_text(base_img, msg1, font_bold_path, 48, (48, 48, 48), 280)
     base_img = add_centered_text(base_img, msg2, font_medium_path, 22, (120, 120, 120), 580)
@@ -45,7 +45,7 @@ def cd_imagemaker(str_day, yourname):
     #base_img.show()
     #base_img.save(cdimage.png)
 
-def cd_sendmsg(strday, yourname):
+def cd_sendmsg(): #strday, yourname
     img_urls = cd_imagemaker
     image_message = ImageSendMessage(
         original_content_url=img_urls,
