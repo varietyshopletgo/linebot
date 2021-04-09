3da85a93b9342f148f8f3e684a75fddc5d9b4d84
hisui0 <lassloshbf@gmail.com>
2021-04-09 17:34:50 +0900

    fix

--- talkapi.py
+++ talkapi.py
@@ -8,6 +8,6 @@ def talkapi(text):
     client = pya3rt.TalkClient(apikey)
     response = client.talk(text)
     msg = response['results'][0]['reply']
-    snd= Sender(name ="令和市公民館跡地のハムスター", icon_url ="https://reiwacity-linebot.s3-ap-northeast-1.amazonaws.com/reiwa_hamstar.jpg"])
+    snd= Sender(name ="令和市公民館跡地のハムスター", icon_url ="https://reiwacity-linebot.s3-ap-northeast-1.amazonaws.com/reiwa_hamstar.jpg")
     text_message = TextSendMessage(msg, sender=snd)
     return text_message
