e75896f74ea8e8978ae8cde203a97d2fb2f15ee1
hisui0 <lassloshbf@gmail.com>
2021-04-09 17:29:04 +0900

    fix

--- talkapi.py
+++ talkapi.py
@@ -1,12 +1,13 @@
 import os
 import pya3rt
 from linebot.models import (
-    TextSendMessage
+    Sender, TextSendMessage
 )
 def talkapi(text):
     apikey = os.environ["TALK_API_KEY"]
     client = pya3rt.TalkClient(apikey)
     response = client.talk(text)
     msg = response['results'][0]['reply']
-    text_message = TextSendMessage(msg, sender=["name"="令和市公民館跡地のハムスター", "icon_url"="https://reiwacity-linebot.s3-ap-northeast-1.amazonaws.com/reiwa_hamstar.jpg"])
+    text_message = TextSendMessage(msg)
+    #sender=Sender(name"="令和市公民館跡地のハムスター", "icon_url"="https://reiwacity-linebot.s3-ap-northeast-1.amazonaws.com/reiwa_hamstar.jpg"])
     return text_message
